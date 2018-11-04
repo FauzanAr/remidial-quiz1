98cbcfa1ab1d3598e3fb3f7a0714cefad1a22c57
Ojan <Ojan@Ojan-PC>
2018-11-04 17:26:53 +0700

    Kelas Layang selesai

---
+++ src/LayangLayang.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ojan
+ */
+public class LayangLayang extends BangunDatar {
+    private int diagonal1,diagonal2;
+    
+    public LayangLayang(int sisi1, int sisi2) {
+        super(sisi1, sisi2);
+    }
+    
+    public int getLuasLayang(){
+        diagonal1 = sisi1;
+        diagonal2 = sisi2;
+        return (int)0.5*diagonal1*diagonal2;
+    }
+    
+    public int getKelilingLayang(){
+        return 2*(sisi1+sisi2);
+    }
+    
+    public String toString(){
+        String print = super.toString();
+        return print += "Layang-layang/Ketupat\nLuas : "+getLuasLayang()+"\nKeliling : "+getKelilingLayang();
+    }
+}
